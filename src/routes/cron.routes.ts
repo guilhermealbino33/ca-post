535b7b54dee953f7a54547050f49bc8c2f5686a7
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-10 17:16:50 -0300

    batch

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { ImageController } from "modules/GoogleCron/imageController";
 import { ProductController } from "modules/GoogleCron/productController";
+import { ProductControllerBatch } from "modules/GoogleCron/productControllerBatch";
 
 const cronRoutes = Router();
 const productController = new ProductController();
+const productControllerBatch = new ProductControllerBatch();
 const imageController = new ImageController();
 
 cronRoutes.post("/", productController.products);
+cronRoutes.post("/batch", productControllerBatch.products);
 cronRoutes.post("/images", imageController.images);
 
 export { cronRoutes };
