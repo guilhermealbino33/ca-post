05d43d1e585e75a9d27cfc42cd1570577a0558b3
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-11 15:59:01 -0300

    productController working with batch

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
-import { ImageController } from "modules/GoogleCron/imageController";
-import { ProductController } from "modules/GoogleCron/productController";
-import { ProductControllerBatch } from "modules/GoogleCron/productControllerBatch";
+import { ImageController } from "modules/Cron/imageController";
+import { ProductController } from "modules/Cron/productController";
 
 const cronRoutes = Router();
 const productController = new ProductController();
-const productControllerBatch = new ProductControllerBatch();
 const imageController = new ImageController();
 
 cronRoutes.post("/", productController.products);
-cronRoutes.post("/batch", productControllerBatch.products);
 cronRoutes.post("/images", imageController.images);
 
 export { cronRoutes };
