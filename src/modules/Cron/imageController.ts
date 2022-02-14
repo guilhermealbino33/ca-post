e4c1dd8d15131ac38926b11012e83bfb0a609304
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-14 18:17:09 -0300

    Update imageController.ts

--- src/modules/Cron/imageController.ts
+++ src/modules/Cron/imageController.ts
@@ -5,18 +5,19 @@ import api, { createToken } from "services/api";
 import { IQueueAdvisorUpdate } from "./models/QueueAdvisorUpdate";
 import queueAdvisorService from "./Services/queueAdvisorService";
 
+const config = {};
+
 class ImageController {
   images = async (req: Request, res: Response) => {
     const headers = {
-      "Content-Type": "multipart/mixed; boundary=changeset",
+      "Content-Type": "application/json",
     };
 
     const queue = await queueAdvisorService.pullOne();
-    let config: any;
     await createToken();
 
     const bodyCodes = {
-      requests: queue.map((item: IQueueAdvisorUpdate, index) => ({
+      requests: queue.map((item: IQueueAdvisorUpdate, index: number) => ({
         id: String(index),
         method: "get",
         url: `/v1/products?$filter=Sku eq '${item.product.data.manufacturerPartNumber}'&$select=ID, Sku`,
@@ -24,12 +25,10 @@ class ImageController {
     };
 
     const codes = await api.post(`/v1/$batch`, JSON.stringify(bodyCodes), {
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
     });
 
-    queue.forEach(async (item: IQueueAdvisorUpdate, index: number) => {
+    queue.forEach(async (item: IQueueAdvisorUpdate) => {
       const { product } = item;
 
       if (!product || !product.data) {
@@ -37,46 +36,40 @@ class ImageController {
         return;
       }
 
-      try {
-        const code = codes.data.responses.find(
-          (code: any) =>
-            code.body.value[0].Sku === product.data.manufacturerPartNumber
-        );
+      const code = codes.data.responses.find(
+        (code: any) =>
+          code.body.value[0].Sku === product.data.manufacturerPartNumber
+      );
 
-        if (product.data.images.length > 0) {
-          product.data.images.forEach(async (image: string, i: number) => {
-            console.log(`code ${code.body.value[0].ID}, image: ${image}`);
+      if (product.data.images.length > 0) {
+        type IBatch = {
+          requests: [id: string, method: string, url: string, body: object];
+        };
+        let config: IBatch;
 
-            config = {
-              requests: [
-                {
-                  id: index,
-                  method: "patch",
-                  url: `/v1/Products(${
-                    code.body.value[0].ID
-                  })/Images(ITEMIMAGEURL${1 + i})`,
-                  body: {
-                    Url: `https://images.qbp.com/imageservice/image/1d59103516e0/prodxl/${image}`,
-                  },
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                },
-              ],
-            };
-          });
+        product.data.images.map(async (image: string, i: number) => {
+          console.log(`code ${code.body.value[0].ID}, image: ${image}`);
+
+          const test = {
+            id: String(i),
+            method: "PUT",
+            url: `/v1/Products(${code.body.value[0].ID})/Images(ITEMIMAGEURL${
+              1 + i
+            })`,
+            body: {
+              Url: `https://images.qbp.com/imageservice/image/1d59103516e0/prodxl/${image}`,
+            },
+          };
+          config.requests.push(test);
+        });
+        try {
+          console.log("external body", config);
+          await api.post("/v1/$batch", JSON.stringify(config), { headers });
+        } catch (e) {
+          console.log(e);
         }
-        console.log("body", config.requests.body);
-      } catch (error) {
-        res.status(400).json(error);
       }
     });
-
-    try {
-      await api.post("/v1/$batch", config, { headers });
-    } catch (e) {
-      console.log(e);
-    }
     res.status(201).json("Job concluded!");
   };
 }
