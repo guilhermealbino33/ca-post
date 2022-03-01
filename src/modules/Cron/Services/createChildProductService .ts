59ba2d57e0b49ac42240824b73249c750b27b470
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-01 10:10:32 -0300

    Update createChildProductService .ts

--- src/modules/Cron/Services/createChildProductService .ts
+++ src/modules/Cron/Services/createChildProductService .ts
@@ -1,105 +1,54 @@
-import { Request, Response } from "express";
 import api from "services/api";
-import { utils } from "utils/utils";
+
+type ChildProduct = {
+  Sku: string;
+  IsParent?: boolean;
+  IsInRelationship?: boolean;
+  ParentProductID: string;
+  VaryBy?: string;
+  Title: string;
+  Description: string;
+  ShortDescription: string;
+  Attributes: object[];
+};
 
 class CreateChildProductService {
-  handle = async (req: Request, res: Response) => {
+  handle = async ({
+    Sku,
+    ParentProductID,
+    VaryBy,
+    Title,
+    Description,
+    ShortDescription,
+    Attributes,
+  }: ChildProduct) => {
     console.log("ENTERED");
-
+    // check for "thirdPartyAllowed" rule
     const headers = {
       "Content-Type": "application/json",
     };
 
-    const { toHtml, removeDuplicatedWordsBetween } = utils;
-
-    const data = {
-      Sku: req.body.data.manufacturerPartNumber,
-      ParentProductID: req.body.data.ProductID /* valor test alterer */,
+    const body = {
+      Sku,
+      ParentProductID,
+      VaryBy,
       IsParent: false,
       IsInRelationship: true,
-      Title: `${req.body.data.brand.name} ${req.body.data.model.name}`,
-      Description: req.body.data.model.description,
-      ShortDescription: toHtml(req.body.data.model.bulletPoints),
-      VaryBy: "Choose Option",
-      Attributes: [
-        {
-          Name: "QBP Code",
-          Value: req.body.data.code,
-        },
-        {
-          Name: "Choose Option",
-          Value: removeDuplicatedWordsBetween(
-            req.body.data.name,
-            req.body.data.model.name,
-            req.body.data.brand.name
-          ),
-        },
-        {
-          Name: "QBP Name",
-          Value: removeDuplicatedWordsBetween(
-            req.body.data.name,
-            req.body.data.model.name,
-            req.body.data.brand.name
-          ),
-        },
-        {
-          Name: "QBP Model",
-          Value: `${req.body.data.brand.name} ${req.body.data.model.name}`,
-        },
-        {
-          Name: "QBP Description",
-          Value: req.body.data.model.description,
-        },
-        {
-          Name: "QBP Short Description",
-          Value: toHtml(req.body.data.model.bulletPoints),
-        },
-        {
-          Name: "QBP Cost",
-          Value: JSON.stringify(req.body.data.basePrice),
-        },
-        {
-          Name: "QBP MAP",
-          Value: JSON.stringify(req.body.data.mapPrice),
-        },
-        {
-          Name: "QBP MSRP",
-          Value: JSON.stringify(req.body.data.msrp),
-        },
-        {
-          Name: "QBP Brand",
-          Value: req.body.data.brand.name,
-        },
-        {
-          Name: "QBP Discontinued",
-          Value: JSON.stringify(req.body.data.discontinued),
-        },
-        {
-          Name: "QBP Thirdpartyallowed",
-          Value: JSON.stringify(req.body.data.thirdPartyAllowed),
-        },
-      ],
-    };
-    type IAttribQBP = {
-      name: string;
-      value: string;
+      Title,
+      Description,
+      ShortDescription,
+      Attributes,
     };
 
-    req.body.data.productAttributes.forEach((attrib: IAttribQBP, i: number) => {
-      const attribute = {
-        Name: `Metafield${1 + i}`,
-        Value: `• ${attrib.name}: ${attrib.value}`,
-      };
-      data.Attributes.push(attribute);
-    });
     try {
-      await api.post(`/v1/Products`, JSON.stringify(data), {
+      await api.post(`/v1/Products`, JSON.stringify(body), {
         headers,
       });
+      return console.log(`Child product created SKU ${body.Sku}`);
     } catch (e) {
       console.log(e);
+      return console.log(`Error creating child product SKU ${body.Sku}`);
     }
-    res.status(201).json(data);
   };
 }
 
