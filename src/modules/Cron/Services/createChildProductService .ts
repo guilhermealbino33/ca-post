b0e7f3585cd74528351708bbf52c0d3e791ab55b
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-02 17:28:36 -0300

    Update createChildProductService .ts

--- src/modules/Cron/Services/createChildProductService .ts
+++ src/modules/Cron/Services/createChildProductService .ts
@@ -1,25 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import api from "services/api";
 
 type ChildProduct = {
   Sku: string;
-  IsParent?: boolean;
-  IsInRelationship?: boolean;
+  IsParent: string;
+  IsInRelationship: string;
   ParentProductID: string;
-  VaryBy: string;
   Title: string;
   Attributes: object;
+  ThirdPartyAllowed: boolean;
 };
 
 class CreateChildProductService {
   handle = async ({
     Sku,
     ParentProductID,
-    VaryBy,
     Title,
+    IsParent,
+    IsInRelationship,
     Attributes,
+    ThirdPartyAllowed,
   }: ChildProduct) => {
-    console.log("ENTERED");
-    // check for "thirdPartyAllowed" rule
     const headers = {
       "Content-Type": "application/json",
     };
@@ -27,20 +28,70 @@ class CreateChildProductService {
     const body = {
       Sku,
       ParentProductID,
-      VaryBy,
-      IsParent: false,
-      IsInRelationship: true,
+      IsParent,
+      IsInRelationship,
       Title,
       Attributes,
     };
 
     try {
-      await api.post(`/v1/Products`, JSON.stringify(body), {
+      const apiTest = await api.post(`/v1/Products`, JSON.stringify(body), {
         headers,
       });
+      console.log("ThirdPartyAllowed", ThirdPartyAllowed);
+      console.log("apiTest", apiTest.data.ID);
+
+      if (ThirdPartyAllowed === true) {
+        try {
+          await api.post(
+            `/v1/$batch`,
+            JSON.stringify({
+              requests: [
+                {
+                  id: 0,
+                  method: "put",
+                  url: `/v1/Products(${apiTest.data.ID})/Labels('eBay Fixed')`,
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": "0",
+                  },
+                },
+                {
+                  id: 1,
+                  method: "put",
+                  url: `/v1/Products(${apiTest.data.ID})/Labels('Incycle.com')`,
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": "0",
+                  },
+                },
+              ],
+            })
+          );
+        } catch (e: any) {
+          console.log("Error", e.response);
+        }
+      } else {
+        const link = `'Incycle.com'`;
+        try {
+          await api.patch(
+            `/v1/Products(${apiTest.data.ID})/Labels(${link})`,
+            null,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                "Content-Length": "0",
+              },
+            }
+          );
+        } catch (e: any) {
+          console.log("Error", e.response.data);
+        }
+      }
+
       return console.log(`Child product created SKU ${body.Sku}`);
-    } catch (e) {
-      console.log(e);
+    } catch (e: any) {
+      console.log("Error", e.response.data);
       return console.log(`Error creating child product SKU ${body.Sku}`);
     }
   };
