abb3fb0bfe5300ff1b129c70d7871820a9869a13
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-05 13:57:04 -0300

    Update updateProductController.ts

--- src/modules/Cron/useCases/updateProductController.ts
+++ src/modules/Cron/useCases/updateProductController.ts
@@ -2,41 +2,64 @@
 import { Request, Response } from "express";
 import api, { createToken } from "services/api";
 
+import { IBatchBody } from "../interfaces/Interfaces";
+
 class UpdateProductController {
   handle = async (req: Request, res: Response) => {
     await createToken();
 
-    const productID = ""; // Verificar para trazer da lista
-    const productSKU = "";
-
     const headers = {
       "Content-Type": "application/json",
     };
+    const batchBody: IBatchBody[] = [];
+    const queue = req.body.Sku;
+    const newSku = req.body.NewSku;
 
-    /** BATCH REQUEST MODEL TESTED ON POSTMAN */
-    const batchBody = {
-      id: "0",
-      method: "put",
-      url: `/v1/Products(${productID})`,
-      body: {
-        Sku: `PARENT-${productSKU}`,
-      },
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const bodyCodes = {
+      requests: queue.map((sku: string, index: number) => ({
+        id: String(index),
+        method: "get",
+        url: `/v1/products?$filter=Sku eq '${sku}'&$select=ID, Sku`,
+      })),
     };
+    const codes = await api.post(`/v1/$batch`, JSON.stringify(bodyCodes), {
+      headers,
+    });
 
+    newSku.forEach(async (newSku: string, index: number) => {
+      const productID = codes.data?.responses[index]?.body.value[0].ID;
+
+      // if (!productID) {
+      //   console.log("Sku not found");
+      //   return;
+      // }
+
+      /** BATCH REQUEST MODEL TESTED ON POSTMAN */
+      const config = {
+        id: String(index),
+        method: "put",
+        url: `/v1/Products(${productID})`,
+        body: {
+          Sku: newSku,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      console.log(`code ${productID}`);
+      batchBody.push(config);
+    });
+    console.log(batchBody);
     try {
       await api.post(
         `/v1/$batch`,
         JSON.stringify({
-          requests: { batchBody },
+          requests: batchBody,
         }),
         {
           headers,
         }
       );
-
       return res.json("Concluded");
     } catch (e: any) {
       return console.log("Error", e.response.data);
