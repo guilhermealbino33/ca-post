f1ce305fae7153781009e375a0b301a9e6198924
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-05 10:42:15 -0300

    Update cron.routes.ts

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
-import { CreateChildProductService } from "modules/Cron/Services/createChildProductService ";
-import { CreateParentProductService } from "modules/Cron/Services/createParentProductService";
 import { ImageController } from "modules/Cron/useCases/imageController";
 import { ProductController } from "modules/Cron/useCases/productController";
+import { UpdateProductController } from "modules/Cron/useCases/updateProductController";
 
 const cronRoutes = Router();
-const createParentProductService = new CreateParentProductService();
-const createChildProductService = new CreateChildProductService();
 const productController = new ProductController();
 const imageController = new ImageController();
+const updateProductController = new UpdateProductController();
 
-cronRoutes.post("/products/child", createChildProductService.handle);
-cronRoutes.post("/products", createParentProductService.handle);
+cronRoutes.post("/products/update", updateProductController.handle);
 cronRoutes.post("/attributes", productController.handle);
 cronRoutes.post("/images", imageController.handle);
 
