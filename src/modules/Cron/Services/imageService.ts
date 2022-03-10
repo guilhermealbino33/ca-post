d709dde6e13949ae2f396c17d63bdf7d286cfed8
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-10 15:16:52 -0300

    Create imageService.ts

---
+++ src/modules/Cron/Services/imageService.ts
@@ -0,0 +1,33 @@
+import { IBatchBody } from "../interfaces/Interfaces";
+
+class ImageService {
+  handle = (childProductId: string, Images: string[]): IBatchBody[] => {
+    const config: IBatchBody[] = [];
+
+    let count = 0;
+    function incrementIndex() {
+      // eslint-disable-next-line no-plusplus
+      return count++;
+    }
+
+    Images.map(async (image: string, i: number) => {
+      const placementName = `'ITEMIMAGEURL${1 + i}'`;
+
+      const body = {
+        id: `image${incrementIndex()}`,
+        method: "patch",
+        url: `/v1/Images(ProductID=${childProductId}, PlacementName=${placementName})`,
+        body: {
+          Url: `https://images.qbp.com/imageservice/image/1d59103516e0/prodxl/${image}`,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      config.push(body);
+    });
+
+    return config;
+  };
+}
+export { ImageService };
