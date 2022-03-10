986f825e9bb56151f88989bfde753c7ebdae07f8
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-10 13:57:40 -0300

    createProduct route removed

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { CreateProductController } from "modules/Cron/useCases/createProductController";
 import { ImageController } from "modules/Cron/useCases/imageController";
 import { ProductController } from "modules/Cron/useCases/productController";
 import { UpdateProductController } from "modules/Cron/useCases/updateProductController";
@@ -8,9 +7,7 @@ const cronRoutes = Router();
 const productController = new ProductController();
 const imageController = new ImageController();
 const updateProductController = new UpdateProductController();
-const createProductController = new CreateProductController();
 
-cronRoutes.post("/products/create", createProductController.handle);
 cronRoutes.post("/products/update", updateProductController.handle);
 cronRoutes.post("/attributes", productController.handle);
 cronRoutes.post("/images", imageController.handle);
