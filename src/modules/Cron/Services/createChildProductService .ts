6db5f4a8919c1f7c6db7f383ad0dcf0cfe025937
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-14 13:21:52 -0300

    Update createChildProductService .ts

--- src/modules/Cron/Services/createChildProductService .ts
+++ src/modules/Cron/Services/createChildProductService .ts
@@ -1,13 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import api from "services/api";
-
 import { IBatchBody } from "../interfaces/Interfaces";
 
 type ChildProduct = {
   Sku: string;
   IsParent: string;
   IsInRelationship: string;
-  ParentSku?: string;
+  ParentSku: string;
   ParentProductID: string;
   Title: string;
   Attributes: object;
@@ -16,32 +13,24 @@ type ChildProduct = {
 };
 
 class CreateChildProductService {
-  handle = async ({
-    Sku,
-    ParentProductID,
-    Title,
-    IsParent,
-    IsInRelationship,
-    Attributes,
-    ThirdPartyAllowed,
-    Images,
-  }: ChildProduct) => {
+  handle = (childProduct: ChildProduct, index: number): IBatchBody => {
+    const {
+      Sku,
+      IsParent,
+      IsInRelationship,
+      ParentSku,
+      ParentProductID,
+      Title,
+      Attributes,
+    } = childProduct;
     const headers = {
       "Content-Type": "application/json",
     };
 
-    let count = 0;
-    function incrementIndex() {
-      // eslint-disable-next-line no-plusplus
-      return count++;
-    }
-
-    const batchBody: IBatchBody[] = [];
-    const batchProduct: IBatchBody[] = [];
-
     const body = {
       Sku,
       ParentProductID,
+      ParentSku,
       IsParent,
       IsInRelationship,
       Title,
@@ -49,112 +38,14 @@ class CreateChildProductService {
     };
 
     const config = {
-      id: String(incrementIndex()),
+      id: String(index),
       method: "post",
       url: "/v1/Products",
       body,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
     };
-    batchProduct.push(config);
-    // console.log("batchProduct", batchProduct);
-    try {
-      const childProduct = await api.post(
-        `/v1/$batch`,
-        JSON.stringify({ requests: batchProduct }),
-        {
-          headers,
-        }
-      );
 
-      if (ThirdPartyAllowed === true) {
-        try {
-          await api.post(
-            `/v1/$batch`,
-            JSON.stringify({
-              requests: [
-                {
-                  id: "0",
-                  method: "put",
-                  url: `/v1/ProductLabels(ProductID=${childProduct.data.responses[0].body.ID}, Name='eBay Fixed')`,
-                  headers: {
-                    "Content-Type": "application/json",
-                    "Content-Length": "0",
-                  },
-                },
-                {
-                  id: "1",
-                  method: "put",
-                  url: `/v1/ProductLabels(ProductID=${childProduct.data.responses[0].body.ID}, Name='Incycle.com')`,
-                  headers: {
-                    "Content-Type": "application/json",
-                    "Content-Length": "0",
-                  },
-                },
-              ],
-            }),
-            { headers }
-          );
-        } catch (e: any) {
-          console.log(
-            `Error creating child product SKU ${body.Sku}`,
-            e.response.data
-          );
-        }
-      } else {
-        try {
-          await api.patch(
-            `/v1/Products(${childProduct.data.responses[0].body.ID})/Labels('Incycle.com')`,
-            null,
-            {
-              headers: {
-                "Content-Type": "application/json",
-                "Content-Length": "0",
-              },
-            }
-          );
-        } catch (e: any) {
-          console.log("Error", e.response.data);
-        }
-      }
-
-      if (Images.length > 0) {
-        Images.map(async (image: string, i: number) => {
-          const placementName = `'ITEMIMAGEURL${1 + i}'`;
-          const config = {
-            id: String(incrementIndex()),
-            method: "patch",
-            url: `/v1/Images(ProductID=${childProduct.data.responses[0].body.ID}, PlacementName=${placementName})`,
-            body: {
-              Url: `https://images.qbp.com/imageservice/image/1d59103516e0/prodxl/${image}`,
-            },
-            headers: {
-              "Content-Type": "application/json",
-            },
-          };
-          batchBody.push(config);
-        });
-        try {
-          await api.post(
-            `/v1/$batch`,
-            JSON.stringify({ requests: batchBody }),
-            {
-              headers,
-            }
-          );
-        } catch (e) {
-          console.log(e);
-        }
-      }
-
-      return console.log(`Child product created SKU ${body.Sku}`);
-    } catch (e: any) {
-      return console.log(
-        `Error creating child product SKU ${body.Sku}`,
-        e.response.data
-      );
-    }
+    return config;
   };
 }
 
