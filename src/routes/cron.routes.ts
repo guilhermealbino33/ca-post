48979005402fa0ac8c10947e36badd0970a62ac2
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-16 18:13:26 -0300

    label route added

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ImageController } from "modules/Cron/useCases/imageController";
+import { LabelController } from "modules/Cron/useCases/labelController";
 import { ProductController } from "modules/Cron/useCases/productController";
 import { UpdateProductController } from "modules/Cron/useCases/updateProductController";
 
@@ -7,9 +8,11 @@ const cronRoutes = Router();
 const productController = new ProductController();
 const imageController = new ImageController();
 const updateProductController = new UpdateProductController();
+const labelController = new LabelController();
 
 cronRoutes.post("/products/update", updateProductController.handle);
 cronRoutes.post("/attributes", productController.handle);
 cronRoutes.post("/images", imageController.handle);
+cronRoutes.post("/labels", labelController.handle);
 
 export { cronRoutes };
