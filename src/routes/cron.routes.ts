4478f096445b8d4b9745890893467ae3efb4e0a2
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-31 18:01:22 -0300

    categoriesController

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ImageController } from "modules/Cron/useCases/imageController";
 import { LabelController } from "modules/Cron/useCases/labelController";
+import { ProductCategoryController } from "modules/Cron/useCases/productCategoryController";
 import { ProductController } from "modules/Cron/useCases/productController";
 import { UpdateProductController } from "modules/Cron/useCases/updateProductController";
 
@@ -9,8 +10,10 @@ const productController = new ProductController();
 const imageController = new ImageController();
 const updateProductController = new UpdateProductController();
 const labelController = new LabelController();
+const categoriesController = new ProductCategoryController();
 
 cronRoutes.post("/products/update", updateProductController.handle);
+cronRoutes.post("/categories", categoriesController.handle);
 cronRoutes.post("/attributes", productController.handle);
 cronRoutes.post("/images", imageController.handle);
 cronRoutes.post("/labels", labelController.handle);
