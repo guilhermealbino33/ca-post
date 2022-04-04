6585db8601b4d966a45ecd8bf6e8f16c47e12c53
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-04 16:16:43 -0300

    Update categoryController.ts

--- src/modules/Cron/useCases/categoryController.ts
+++ src/modules/Cron/useCases/categoryController.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Request, Response } from "express";
 import api, { createToken } from "services/api";
+import { v4 as uuidV4 } from "uuid";
 
 import { IBatchBody } from "../interfaces/Interfaces";
 import { IQueueAdvisorUpdate } from "../models/QueueAdvisorUpdate";
@@ -20,7 +22,7 @@ class CategoryController {
     };
     const batchBody: IBatchBody[] = [];
 
-    const queue = await queueAdvisorService.pullQueue(60);
+    const queue = await queueAdvisorService.pullQueue(100);
 
     const sku = queue.map(
       (item) => item.product?.data.manufacturerPartNumber ?? item.product?.code
@@ -33,21 +35,33 @@ class CategoryController {
       const productID = codes.data?.responses[i]?.body.value[0]?.ID;
 
       if (!productID || !product) {
-        console.log(`*************** Sku ${sku} not found ***************`);
+        console.log(`*************** Sku not found ***************`);
         return;
       }
       const data = categoryService.handle(product);
+      console.log(productID);
+
+      const config = {
+        id: String(uuidV4()),
+        method: "post",
+        url: `/v1/Products(${productID})/UpdateAttributes`,
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      batchBody.push(config);
     });
     try {
-      // await api.post(
-      //   `/v1/$batch`,
-      //   JSON.stringify({
-      //     requests: batchBody,
-      //   }),
-      //   {
-      //     headers,
-      //   }
-      // );
+      await api.post(
+        `/v1/$batch`,
+        JSON.stringify({
+          requests: batchBody,
+        }),
+        {
+          headers,
+        }
+      );
       return res.json("Concluded");
     } catch (e: any) {
       return console.log("Error", e);
