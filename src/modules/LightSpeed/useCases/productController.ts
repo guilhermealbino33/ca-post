972a5f12f8ffb897830b0151c8a2aae578f97a53
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-11 17:41:23 -0300

    Create productController.ts

---
+++ src/modules/LightSpeed/useCases/productController.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import api, { getAuthToken } from "services/LightSpeed/api";
+
+class ProductController {
+  handle = async (req: Request, res: Response) => {
+    const token = await getAuthToken();
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+
+    try {
+      const apiReturn = await api.get(
+        `/API/V3/Account/${process.env.ACCOUNT_ID}/Category.json`,
+        {
+          headers,
+        }
+      );
+      return res.json(apiReturn.data);
+    } catch (e: any) {
+      return console.log("Error", e);
+    }
+  };
+}
+export { ProductController };
