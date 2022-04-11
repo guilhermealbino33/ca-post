23bb35b02878a693ad1037170558cef21ed7ef5c
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-11 17:41:15 -0300

    route working - service not working

--- src/modules/Lightspeed/useCases/categoryController.ts
+++ src/modules/Lightspeed/useCases/categoryController.ts
@@ -1,67 +1,40 @@
-/* eslint-disable no-plusplus */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { Request, Response } from "express";
-import { IBatchBody } from "modules/Cron/interfaces/Interfaces";
-import { GetProductsBySkuService } from "modules/Cron/services/getProductsBySkuService";
 import api, { getAuthToken } from "services/LightSpeed/api";
 import queueAdvisorService from "services/Queue";
 import { IQueueAdvisor } from "services/Queue/interfaces/interfaces";
-import { v4 as uuidV4 } from "uuid";
 
 import { CategoryService } from "../services/categoryService";
 
 class CategoryController {
   handle = async (req: Request, res: Response) => {
+    const token = await getAuthToken();
     const categoryService = new CategoryService();
-    await getAuthToken();
-    const getProductsBySkuService = new GetProductsBySkuService();
-
+    const queue = await queueAdvisorService.pullCategoryQueue(1);
+    const data: any = [];
     const headers = {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     };
-    const batchBody: IBatchBody[] = [];
 
-    const queue = await queueAdvisorService.pullCategoryQueue(100);
-
-    const sku = queue.map(
-      (item) => item.product?.data.manufacturerPartNumber ?? item.product?.code
-    );
-
-    const codes = await getProductsBySkuService.handle(sku);
-
-    queue.forEach(async (item: IQueueAdvisor, i: number) => {
+    queue.forEach(async (item: IQueueAdvisor) => {
       const { product } = item;
-      const productID = codes.data?.responses[i]?.body.value[0]?.ID;
 
-      if (!productID || !product) {
+      if (!product) {
         console.log(`*************** Sku not found ***************`);
         return;
       }
-      const data = categoryService.handle(product);
-      console.log(productID);
-
-      const config = {
-        id: String(uuidV4()),
-        method: "post",
-        url: `/API/Account/{accountID}/Category.json`,
-        body: data,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      batchBody.push(config);
+      data.push(categoryService.handle(product));
     });
     try {
-      // await api.post(
-      //   `/v1/$batch`,
-      //   JSON.stringify({
-      //     requests: batchBody,
-      //   }),
-      //   {
-      //     headers,
-      //   }
-      // );
+      await api.post(
+        `/API/V3/Account/${process.env.ACCOUNT_ID}/Category.json`,
+        data,
+        {
+          headers,
+        }
+      );
       return res.json("Concluded");
     } catch (e: any) {
       return console.log("Error", e);
