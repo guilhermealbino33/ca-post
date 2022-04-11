00416b6f84f807a06aa554f111bfbfbebed72d5f
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-11 14:14:50 -0300

    Create api.ts

---
+++ src/services/LightSpeed/api.ts
@@ -0,0 +1,46 @@
+/* eslint-disable func-names */
+import axios, { AxiosResponse } from "axios";
+
+interface ITokenResponse {
+  access_token: string;
+  expires_in: string;
+  token_type: string;
+  scope: string;
+}
+
+const api = axios.create({
+  baseURL: "https://api.lightspeedapp.com",
+});
+
+export const getAuthToken = async (): Promise<string> => {
+  const config = {
+    client_id: process.env.LS_CLIENT_ID,
+    client_secret: process.env.LS_CLIENT_SECRET,
+    grant_type: process.env.LS_GRANT_TYPE,
+    refresh_token: process.env.LS_REFRESH_TOKEN,
+  };
+
+  //   return await axios({
+  //     method: "POST",
+  //     url,
+  //     data: config,
+  //   })
+  //     .then(function (response: AxiosResponse<ITokenResponse>) {
+  //       return response.data.access_token;
+  //     })
+  //     .catch(function (response) {
+  //       logging.error("Failed to get LSH token!", response);
+  //       return null;
+  //     });
+  // };
+  return api
+    .post("/oauth/access_token.php", config)
+    .then(function (response: AxiosResponse<ITokenResponse>) {
+      return response.data.access_token;
+    })
+    .catch(function (response) {
+      console.log("Failed to get LightSpeed token!", response);
+      return "";
+    });
+};
+export default api;
