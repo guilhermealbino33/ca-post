30db1abb4ca114d0eb5d19e3f40c52e40554ea1c
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-12 17:29:39 -0300

    trouble with array values outside a loop

--- src/modules/LightSpeed/services/getItemsByManufacturerSkuService.ts
+++ src/modules/LightSpeed/services/getItemsByManufacturerSkuService.ts
@@ -1,12 +1,27 @@
-import api from "services/ChannelAdvisor/api";
+import api from "services/LightSpeed/api";
 
 class GetItemsByManufacturerSkuService {
-  async handle(sku: string) {
-    const codes = await api.get(
-      `API/V3/Account/${process.env.ACCOUNT_ID}/Item.json?manufacturerSku=${sku}`
-    );
-    console.log("codes", codes);
-    return codes;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async handle(manufacturerSku: string, token: any) {
+    console.log("manufacturerSku", manufacturerSku);
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const item = await api.get(
+        `API/V3/Account/${process.env.ACCOUNT_ID}/Item.json?manufacturerSku=${manufacturerSku}`,
+        { headers }
+      );
+      const itemId = item.data?.Item?.itemID;
+      if (itemId && itemId !== undefined) {
+        return `${itemId}`;
+      }
+      return "";
+    } catch (error) {
+      console.log("error");
+      return error;
+    }
   }
 }
 
--- src/modules/LightSpeed/useCases/updateItemController.ts
+++ src/modules/LightSpeed/useCases/updateItemController.ts
@@ -10,25 +10,33 @@ class UpdateItemController {
   handle = async (req: Request, res: Response) => {
     const getItemsByManufacturerSkuService =
       new GetItemsByManufacturerSkuService();
+
     const token = await getAuthToken();
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     };
-
-    const queue = await queueService.pullImageQueue(1);
-
+    const queue = await queueService.pullImageQueue(50);
+    const itemsId: string[] = [];
+    let array;
     queue.forEach(async (item: IQueueInterface) => {
       const { product } = item;
-
-      getItemsByManufacturerSkuService.handle(
-        product.data.manufacturerPartNumber
+      if (!product) {
+        return;
+      }
+      const itemId = await getItemsByManufacturerSkuService.handle(
+        product.data.manufacturerPartNumber,
+        token
       );
+      if (itemId !== "") {
+        itemsId.push(String(itemId));
+        array = itemsId;
+        console.log("itemsId", array);
+      }
     });
-
     try {
       // const apiReturn = await api.get(
-      //   `/API/V3/Account/${process.env.ACCOUNT_ID}/Item.json`,
+      //   `/API/V3/Account/{accountID}/Item/${itemID}.json`,
       //   {
       //     headers,
       //   }
