957628ebdecd4b98afc47f688d5bd3172927ae6a
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-12 10:13:05 -0300

    controller renamed

--- src/routes/lightspeed.routes.ts
+++ src/routes/lightspeed.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { CategoryController } from "modules/LightSpeed/useCases/categoryController";
-import { ProductController } from "modules/LightSpeed/useCases/productController";
+import { ProductController } from "modules/LightSpeed/useCases/itemController";
 
 const lightSpeedRoutes = Router();
 const categoryController = new CategoryController();
 const productController = new ProductController();
 
 lightSpeedRoutes.post("/categories", categoryController.handle);
-lightSpeedRoutes.get("/products", productController.handle);
+lightSpeedRoutes.get("/items", productController.handle);
 
 export { lightSpeedRoutes };
