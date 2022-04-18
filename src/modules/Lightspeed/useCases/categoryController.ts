1e9f15f76dd76761c92a894ef0c5dc928e91c5f8
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-18 18:22:26 -0300

    Working on Categories

--- src/modules/Lightspeed/useCases/categoryController.ts
+++ src/modules/Lightspeed/useCases/categoryController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-loop-func */
 import { Request, Response } from "express";
 import api, { getAuthToken } from "services/LightSpeed/api";
@@ -9,52 +10,81 @@ import { IProductInterface } from "../../QBP/models/ProductInterface";
 class CategoryController {
   handle = async (req: Request, res: Response) => {
     const token = await getAuthToken();
-    const data: any = [];
+    const createdCategory: any = [];
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     };
-
+    let currentCode;
     const categoriesMock = categories.categories;
 
-    categoriesMock.forEach((category) => {
-      if (category.parentCode === "g0") {
-        data.push({
-          name: category.name,
+    interface ICategory {
+      code?: string;
+      name: string;
+      parentCode: any;
+      childCodes?: string[];
+    }
+
+    // function recurse(category: ICategory | any, index: number) {
+    //   if (category?.childCodes.length > 0) {
+    //     data.push({
+    //       Name: category.name,
+    //     });
+    //     const i = index;
+    //     const child = categoriesMock.find(
+    //       ({ code }) => code === category.parentCode
+    //     );
+    //     console.log("child", child);
+
+    //     recurse(child, i);
+    //   }
+    // }
+
+    async function returnParentID(body: any, parentCode: any): Promise<any> {
+      try {
+        const parent = await api.post(
+          `/API/V3/Account/${process.env.ACCOUNT_ID}/Category.json`,
+          body,
+          {
+            headers,
+          }
+        );
+        const parentID = parent.data.Category.categoryID;
+        if (!parentID || parentID === undefined) {
+          return "0";
+        }
+        createdCategory.push({
+          parentID,
+          parentCode,
         });
+        return parentID.data.Category.parentID;
+      } catch (error) {
+        console.log(error);
+        return "error";
       }
+    }
 
-      // switch (category.parentCode) {
-      //   case "g0":
-      //     data.push({
-      //       name: category.name,
-      //       // fullPathName: category.name, // fazer breadcrumbs
-      //       parentID: category.parentCode,
-      //     });
+    let parentCode: any | string;
 
-      //     break;
-      //   case "cw-special-categories":
-      //     data.push({
-      //       name: category.name,
-      //       // fullPathName: category.name, // fazer breadcrumbs
-      //       parentID: category.parentCode,
-      //     });
+    for (const category of categoriesMock) {
+      console.log("parentCode", parentCode);
+      const data = {
+        name: category.name,
+        parentID: parentCode,
+      };
+      console.log("DATA", data);
+      parentCode = await returnParentID(data, category.parentCode);
+      const newParentCode = createdCategory.find(
+        (parentCode) => parentCode === category.parentCode
+      );
+      console.log("newParentCode", newParentCode);
+    }
 
-      //     break;
+    /**
+     * Armazenar os categories criados em um array com o ID vindo da lightspeed e o ID do JSON
+     * Depois parentCode, fazer um find na array criada buscando o id desejado
+     */
 
-      //   default:
-      //     break;
-      // }
-
-      // while (category.parentCode === "g0") {
-      //   data.push({
-      //     name: category.name,
-      //     // fullPathName: category.name, // fazer breadcrumbs
-      //     parentID: category.parentCode,
-      //   });
-      // }
-    });
-    console.log("data", data);
     try {
       // await api.post(
       //   `/API/V3/Account/${process.env.ACCOUNT_ID}/Category.json`,
