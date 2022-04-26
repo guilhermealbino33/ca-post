4c06527a571574bcf26bc702949732ea925ac32f
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-26 14:48:33 -0300

    fix: fixed to use the right path

--- src/modules/LightSpeed/useCases/getItemsController.ts
+++ src/modules/LightSpeed/useCases/getItemsController.ts
@@ -1,20 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
-import api, { getAuthToken } from "services/LightSpeed/api";
+import api, { getToken } from "services/LightSpeed/api";
 
 class GetItemsController {
   handle = async (req: Request, res: Response) => {
-    const token = await getAuthToken();
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    };
-
+    const token = await getToken();
     try {
       const apiReturn = await api.get(
         `/API/V3/Account/${process.env.ACCOUNT_ID}/Item.json`,
         {
-          headers,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
         }
       );
       return res.json(apiReturn.data);
