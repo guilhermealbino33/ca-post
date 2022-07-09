87e0fce9e491bfe94da77594c9408a72702ccf3f
Guilherme Albino <guilhermealbino33@gmail.com>
2022-07-09 13:47:21 -0300

    Update attributesService.ts

--- src/modules/Cron/Services/attributesService.ts
+++ src/modules/Cron/Services/attributesService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import { utils } from "utils/utils";
 
 import { IProductInterface } from "../../QBP/models/ProductInterface";
@@ -74,12 +75,82 @@ class AttributesService {
       },
     };
 
-    product.data.productAttributes.forEach((attrib: IAttribQBP, i: number) => {
+    const arrayValues: IAttribQBP[] = [];
+
+    const getDuplicatedElement = (attribute: IAttribQBP) => {
+      return product.data.productAttributes.filter(
+        (item) => item.name === attribute.name
+      );
+    };
+    const duplicatedTarget: IAttribQBP[] = [];
+    // esse for deveria ser em cima dos valores externos?
+    product.data.productAttributes.forEach((attribute) => {
+      const isDuplicatedTarget = duplicatedTarget.find(
+        (item) => item.name === attribute.name
+      );
+      if (isDuplicatedTarget) {
+        console.log("entrou isDuplicatedTarget");
+        return;
+      }
+
+      let duplicatedTargets = getDuplicatedElement(attribute);
+
+      console.log(
+        "duplicatedTargets",
+        product.data.productAttributes.filter(
+          (item) => item.name === attribute.name
+        )
+      );
+
+      if (duplicatedTargets) {
+        const names = product.data.productAttributes.map(
+          (attributes) => attributes.name
+        );
+
+        const findDuplicates = (attrib: string[]) => {
+          const sorted_arr = attrib.slice().sort();
+          const results = [];
+          for (let i = 0; i < sorted_arr.length - 1; i++) {
+            if (sorted_arr[i + 1] === sorted_arr[i]) {
+              results.push(sorted_arr[i]);
+            }
+          }
+          return results;
+        };
+
+        const duplicates = findDuplicates(names);
+
+        const duplicatesValues = product.data.productAttributes.filter(
+          (attributes) => attributes.name === duplicates[0]
+        );
+        const value = duplicatesValues.map((attributes) => attributes.value);
+        const name = duplicates[0] as string;
+
+        const attribute = {
+          name,
+          value: value.join(", "),
+        };
+
+        arrayValues.push(attribute);
+        duplicatedTargets.forEach((item) => {
+          duplicatedTarget.push(item);
+        });
+        duplicatedTargets = [];
+      } else {
+        console.log("entrou else");
+        arrayValues.push(attribute);
+      }
+    });
+
+    arrayValues.map((attrib) => console.log(attrib));
+
+    arrayValues.forEach((attrib: IAttribQBP, i: number) => {
       const name = {
         Name: `MetafieldName${1 + i}`,
         Value: attrib.name,
       };
       data.Value.Attributes.push(name);
+
       const value = {
         Name: `MetafieldValue${1 + i}`,
         Value: attrib.value,
